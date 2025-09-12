6108edd2853a8be262bd9f472d00d78ab45c44f6
qusai-rasheed <qusai@qmortgage.ai>
2025-09-12 16:24:36 -0700

    streamlit_app.py
    import streamlit as st
    from snowflake.snowpark import Session
    
    # Create connection using Streamlit's connection
    cnx = st.connection("snowflake")
    session = cnx.session()
    
    # Now you can use the session
    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,17 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import streamlit as st
+from snowflake.snowpark import Session
+
+# Create connection using Streamlit's connection
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+# Now you can use the session
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+
 # Write directly to the app
 st.title("Customize your smoothie! 🥤")
 st.write('Choose the fruits you want to add to your smoothie!')
